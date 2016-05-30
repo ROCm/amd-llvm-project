259bf1ba770c14d855f00776cfb54319d49c8570
Jack Chung <whchung@gmail.com>
2016-05-30 22:05:36 +0800

    [Basic][Sema][GridLaunch] Fix typedef issue with grid_launch_parm as first parameter
    Adopted from Clang 3.5-based HCC-clang frontend.
    
    https://github.com/RadeonOpenCompute/hcc-clang/commit/d37fbad
    
    - include/clang/Basic/DiagnosticSemaKinds.td : Patched as-is
    - lib/Sema/SemaDecl.cpp : Patched as-is

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9183,7 +9183,7 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
   // Place this check before returning FunctionTemplate
   if (NewFD->hasAttr<HCGridLaunchAttr>()) {
     for(auto PVD : NewFD->parameters()) {
-      QualType PT = PVD->getType();
+      QualType PT = PVD->getType().getCanonicalType();
       // Check if first parameter has grid_launch_parm type
       if(PT->getAs<ReferenceType>()) {
         Diag(PVD->getLocation(), diag::err_hc_grid_launch_ref);
@@ -9193,7 +9193,7 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
       else if (PVD == *(NewFD->param_begin())) {
         std::size_t found = PT.getAsString().find("grid_launch_parm");
         if (found == std::string::npos) {
-          Diag(PVD->getLocation(), diag::err_hc_grid_launch_parm);
+          Diag(PVD->getLocation(), diag::err_hc_grid_launch_parm) << PT.getAsString();
           D.setInvalidType();
         }
       }
