9eaa14bdcedade1fd665addff85904e05c7bbb2b
Jack Chung <whchung@gmail.com>
2016-07-13 21:33:35 +0800

    [Driver] add tonga as a potential AMDGPU target

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -9400,12 +9400,15 @@ void gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
         LinkerArgString.append("hawaii");
       } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:1")) {
         LinkerArgString.append("carrizo");
+      } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:2")) {
+        LinkerArgString.append("tonga");
       } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:3")) {
         LinkerArgString.append("fiji");
       } else if (AMDGPUTarget.equals("fiji") ||
                  AMDGPUTarget.equals("kaveri") ||
                  AMDGPUTarget.equals("carrizo") ||
-                 AMDGPUTarget.equals("hawaii")) {
+                 AMDGPUTarget.equals("hawaii") ||
+                 AMDGPUTarget.equals("tonga")) {
         // directly use GPU family
         LinkerArgString.append(AMDGPUTarget);
       } else {
@@ -11325,12 +11328,15 @@ void HCC::CXXAMPLink::ConstructJob(Compilation &C, const JobAction &JA,
       LinkerArgString.append("hawaii");
     } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:1")) {
       LinkerArgString.append("carrizo");
+    } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:2")) {
+      LinkerArgString.append("tonga");
     } else if (AMDGPUTarget.equals("AMD:AMDGPU:8:0:3")) {
       LinkerArgString.append("fiji");
     } else if (AMDGPUTarget.equals("fiji") ||
                AMDGPUTarget.equals("kaveri") ||
                AMDGPUTarget.equals("carrizo") ||
-               AMDGPUTarget.equals("hawaii")) {
+               AMDGPUTarget.equals("hawaii") ||
+               AMDGPUTarget.equals("tonga")) {
       // directly use GPU family
       LinkerArgString.append(AMDGPUTarget);
     } else {
