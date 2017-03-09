b3e1cbe1c58ad25c33c9159d6a577f967a4616f2
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2017-03-09 01:13:38 -0500

    Update cuda lit tests for addr space change

--- clang/test/CodeGenCUDA/address-spaces.cu
+++ clang/test/CodeGenCUDA/address-spaces.cu
@@ -9,8 +9,7 @@
 // CHECK: @i = addrspace(1) externally_initialized global
 __device__ int i;
 
-// AMDGCN: @j = addrspace(2) externally_initialized global
-// NVPTX: @j = addrspace(4) externally_initialized global
+// CHECK: @j = addrspace(4) externally_initialized global
 __constant__ int j;
 
 // CHECK: @k = addrspace(3) global
@@ -29,21 +28,17 @@ struct MyStruct {
 // CHECK: @b = addrspace(3) global float undef
 
 __device__ void foo() {
-  // NVPTX: load i32, i32* addrspacecast (i32 addrspace(1)* @i to i32*)
-  // AMDGCN: load i32, i32 addrspace(4)* addrspacecast (i32 addrspace(1)* @i to i32 addrspace(4)*)
+  // CHECK: load i32, i32* addrspacecast (i32 addrspace(1)* @i to i32*)
   i++;
 
-  // NVPTX: load i32, i32* addrspacecast (i32 addrspace(4)* @j to i32*)
-  // AMDGCN: load i32, i32 addrspace(4)* addrspacecast (i32 addrspace(2)* @j to i32 addrspace(4)*)
+  // CHECK: load i32, i32* addrspacecast (i32 addrspace(4)* @j to i32*)
   j++;
 
-  // NVPTX: load i32, i32* addrspacecast (i32 addrspace(3)* @k to i32*)
-  // AMDGCN: load i32, i32 addrspace(4)* addrspacecast (i32 addrspace(3)* @k to i32 addrspace(4)*)
+  // CHECK: load i32, i32* addrspacecast (i32 addrspace(3)* @k to i32*)
   k++;
 
   __shared__ int lk;
-  // NVPTX: load i32, i32* addrspacecast (i32 addrspace(3)* @_ZZ3foovE2lk to i32*)
-  // AMDGCN: load i32, i32 addrspace(4)* addrspacecast (i32 addrspace(3)* @_ZZ3foovE2lk to i32 addrspace(4)*)
+  // CHECK: load i32, i32* addrspacecast (i32 addrspace(3)* @_ZZ3foovE2lk to i32*)
   lk++;
 }
 
@@ -54,8 +49,7 @@ __device__ void func0() {
   ap->data2 = 2;
 }
 // CHECK-LABEL: define void @_Z5func0v()
-// NVPTX: store %struct.MyStruct* addrspacecast (%struct.MyStruct addrspace(3)* @_ZZ5func0vE1a to %struct.MyStruct*), %struct.MyStruct** %ap
-// AMDGCN: store %struct.MyStruct addrspace(4)* addrspacecast (%struct.MyStruct addrspace(3)* @_ZZ5func0vE1a to %struct.MyStruct addrspace(4)*), %struct.MyStruct addrspace(4)* addrspace(4)* %ap
+// CHECK: store %struct.MyStruct* addrspacecast (%struct.MyStruct addrspace(3)* @_ZZ5func0vE1a to %struct.MyStruct*), %struct.MyStruct** %ap
 
 __device__ void callee(float *ap) {
   *ap = 1.0f;
@@ -66,8 +60,7 @@ __device__ void func1() {
   callee(&a); // implicit cast from parameters
 }
 // CHECK-LABEL: define void @_Z5func1v()
-// NVPTX: call void @_Z6calleePf(float* addrspacecast (float addrspace(3)* @_ZZ5func1vE1a to float*))
-// AMDGCN: call void @_Z6calleePf(float addrspace(4)* addrspacecast (float addrspace(3)* @_ZZ5func1vE1a to float addrspace(4)*))
+// CHECK: call void @_Z6calleePf(float* addrspacecast (float addrspace(3)* @_ZZ5func1vE1a to float*))
 
 __device__ void func2() {
   __shared__ float a[256];
@@ -75,16 +68,14 @@ __device__ void func2() {
   *ap = 1.0f;
 }
 // CHECK-LABEL: define void @_Z5func2v()
-// NVPTX: store float* getelementptr inbounds ([256 x float], [256 x float]* addrspacecast ([256 x float] addrspace(3)* @_ZZ5func2vE1a to [256 x float]*), i32 0, i32 128), float** %ap
-// AMDGCN: store float addrspace(4)* getelementptr inbounds ([256 x float], [256 x float] addrspace(4)* addrspacecast ([256 x float] addrspace(3)* @_ZZ5func2vE1a to [256 x float] addrspace(4)*), i64 0, i64 128), float addrspace(4)* addrspace(4)* %ap
+// CHECK: store float* getelementptr inbounds ([256 x float], [256 x float]* addrspacecast ([256 x float] addrspace(3)* @_ZZ5func2vE1a to [256 x float]*), i{{32|64}} 0, i{{32|64}} 128), float** %ap
 __device__ void func3() {
   __shared__ float a;
   float *ap = reinterpret_cast<float *>(&a); // explicit cast
   *ap = 1.0f;
 }
 // CHECK-LABEL: define void @_Z5func3v()
-// NVPTX: store float* addrspacecast (float addrspace(3)* @_ZZ5func3vE1a to float*), float** %ap
-// AMDGCN: store float addrspace(4)* addrspacecast (float addrspace(3)* @_ZZ5func3vE1a to float addrspace(4)*), float addrspace(4)* addrspace(4)* %ap
+// CHECK: store float* addrspacecast (float addrspace(3)* @_ZZ5func3vE1a to float*), float** %ap
 
 __device__ void func4() {
   __shared__ float a;
@@ -92,15 +83,12 @@ __device__ void func4() {
   *ap = 1.0f;
 }
 // CHECK-LABEL: define void @_Z5func4v()
-// NVPTX: store float* addrspacecast (float addrspace(3)* @_ZZ5func4vE1a to float*), float** %ap
-// AMDGCN: store float addrspace(4)* addrspacecast (float addrspace(3)* @_ZZ5func4vE1a to float addrspace(4)*), float addrspace(4)* addrspace(4)* %ap
+// CHECK: store float* addrspacecast (float addrspace(3)* @_ZZ5func4vE1a to float*), float** %ap
 
 __shared__ float b;
 
 __device__ float *func5() {
   return &b; // implicit cast from a return value
 }
-// NVPTX-LABEL: define float* @_Z5func5v()
-// AMDGCN-LABEL: define float addrspace(4)* @_Z5func5v()
-// NVPTX: ret float* addrspacecast (float addrspace(3)* @b to float*)
-// AMDGCN: ret float addrspace(4)* addrspacecast (float addrspace(3)* @b to float addrspace(4)*)
+// CHECK-LABEL: define float* @_Z5func5v()
+// CHECK: ret float* addrspacecast (float addrspace(3)* @b to float*)
--- clang/test/CodeGenCUDA/device-var-init.cu
+++ clang/test/CodeGenCUDA/device-var-init.cu
@@ -21,8 +21,7 @@ __device__ int d_v;
 __shared__ int s_v;
 // CHECK: @s_v = addrspace(3) global i32 undef,
 __constant__ int c_v;
-// NVPTX: addrspace(4) externally_initialized global i32 0,
-// AMDGCN: addrspace(2) externally_initialized global i32 0,
+// CHECK: addrspace(4) externally_initialized global i32 0,
 
 __device__ int d_v_i = 1;
 // CHECK: @d_v_i = addrspace(1) externally_initialized global i32 1,
@@ -33,14 +32,12 @@ __device__ T d_t;
 __shared__ T s_t;
 // CHECK: @s_t = addrspace(3) global %struct.T undef,
 __constant__ T c_t;
-// NVPTX: @c_t = addrspace(4) externally_initialized global %struct.T zeroinitializer,
-// AMDGCN: @c_t = addrspace(2) externally_initialized global %struct.T zeroinitializer,
+// CHECK: @c_t = addrspace(4) externally_initialized global %struct.T zeroinitializer,
 
 __device__ T d_t_i = {2};
 // CHECK: @d_t_i = addrspace(1) externally_initialized global %struct.T { i32 2 },
 __constant__ T c_t_i = {2};
-// NVPTX: @c_t_i = addrspace(4) externally_initialized global %struct.T { i32 2 },
-// AMDGCN: @c_t_i = addrspace(2) externally_initialized global %struct.T { i32 2 },
+// CHECK: @c_t_i = addrspace(4) externally_initialized global %struct.T { i32 2 },
 
 // empty constructor
 __device__ EC d_ec;
@@ -48,8 +45,7 @@ __device__ EC d_ec;
 __shared__ EC s_ec;
 // CHECK: @s_ec = addrspace(3) global %struct.EC undef,
 __constant__ EC c_ec;
-// NVPTX: @c_ec = addrspace(4) externally_initialized global %struct.EC zeroinitializer,
-// AMDGCN: @c_ec = addrspace(2) externally_initialized global %struct.EC zeroinitializer,
+// CHECK: @c_ec = addrspace(4) externally_initialized global %struct.EC zeroinitializer,
 
 // empty destructor
 __device__ ED d_ed;
@@ -57,16 +53,14 @@ __device__ ED d_ed;
 __shared__ ED s_ed;
 // CHECK: @s_ed = addrspace(3) global %struct.ED undef,
 __constant__ ED c_ed;
-// NVPTX: @c_ed = addrspace(4) externally_initialized global %struct.ED zeroinitializer,
-// AMDGCN: @c_ed = addrspace(2) externally_initialized global %struct.ED zeroinitializer,
+// CHECK: @c_ed = addrspace(4) externally_initialized global %struct.ED zeroinitializer,
 
 __device__ ECD d_ecd;
 // CHECK: @d_ecd = addrspace(1) externally_initialized global %struct.ECD zeroinitializer,
 __shared__ ECD s_ecd;
 // CHECK: @s_ecd = addrspace(3) global %struct.ECD undef,
 __constant__ ECD c_ecd;
-// NVPTX: @c_ecd = addrspace(4) externally_initialized global %struct.ECD zeroinitializer,
-// AMDGCN: @c_ecd = addrspace(2) externally_initialized global %struct.ECD zeroinitializer,
+// CHECK: @c_ecd = addrspace(4) externally_initialized global %struct.ECD zeroinitializer,
 
 // empty templated constructor -- allowed with no arguments
 __device__ ETC d_etc;
@@ -74,14 +68,12 @@ __device__ ETC d_etc;
 __shared__ ETC s_etc;
 // CHECK: @s_etc = addrspace(3) global %struct.ETC undef,
 __constant__ ETC c_etc;
-// NVPTX: @c_etc = addrspace(4) externally_initialized global %struct.ETC zeroinitializer,
-// AMDGCN: @c_etc = addrspace(2) externally_initialized global %struct.ETC zeroinitializer,
+// CHECK: @c_etc = addrspace(4) externally_initialized global %struct.ETC zeroinitializer,
 
 __device__ NCFS d_ncfs;
 // CHECK: @d_ncfs = addrspace(1) externally_initialized global %struct.NCFS { i32 3 }
 __constant__ NCFS c_ncfs;
-// NVPTX: @c_ncfs = addrspace(4) externally_initialized global %struct.NCFS { i32 3 }
-// AMDGCN: @c_ncfs = addrspace(2) externally_initialized global %struct.NCFS { i32 3 }
+// CHECK: @c_ncfs = addrspace(4) externally_initialized global %struct.NCFS { i32 3 }
 
 // Regular base class -- allowed
 __device__ T_B_T d_t_b_t;
@@ -89,8 +81,7 @@ __device__ T_B_T d_t_b_t;
 __shared__ T_B_T s_t_b_t;
 // CHECK: @s_t_b_t = addrspace(3) global %struct.T_B_T undef,
 __constant__ T_B_T c_t_b_t;
-// NVPTX: @c_t_b_t = addrspace(4) externally_initialized global %struct.T_B_T zeroinitializer,
-// AMDGCN: @c_t_b_t = addrspace(2) externally_initialized global %struct.T_B_T zeroinitializer,
+// CHECK: @c_t_b_t = addrspace(4) externally_initialized global %struct.T_B_T zeroinitializer,
 
 // Incapsulated object of allowed class -- allowed
 __device__ T_F_T d_t_f_t;
@@ -98,8 +89,7 @@ __device__ T_F_T d_t_f_t;
 __shared__ T_F_T s_t_f_t;
 // CHECK: @s_t_f_t = addrspace(3) global %struct.T_F_T undef,
 __constant__ T_F_T c_t_f_t;
-// NVPTX: @c_t_f_t = addrspace(4) externally_initialized global %struct.T_F_T zeroinitializer,
-// AMDGCN: @c_t_f_t = addrspace(2) externally_initialized global %struct.T_F_T zeroinitializer,
+// CHECK: @c_t_f_t = addrspace(4) externally_initialized global %struct.T_F_T zeroinitializer,
 
 // array of allowed objects -- allowed
 __device__ T_FA_T d_t_fa_t;
@@ -107,8 +97,7 @@ __device__ T_FA_T d_t_fa_t;
 __shared__ T_FA_T s_t_fa_t;
 // CHECK: @s_t_fa_t = addrspace(3) global %struct.T_FA_T undef,
 __constant__ T_FA_T c_t_fa_t;
-// NVPTX: @c_t_fa_t = addrspace(4) externally_initialized global %struct.T_FA_T zeroinitializer,
-// AMDGCN: @c_t_fa_t = addrspace(2) externally_initialized global %struct.T_FA_T zeroinitializer,
+// CHECK: @c_t_fa_t = addrspace(4) externally_initialized global %struct.T_FA_T zeroinitializer,
 
 
 // Calling empty base class initializer is OK
@@ -117,8 +106,7 @@ __device__ EC_I_EC d_ec_i_ec;
 __shared__ EC_I_EC s_ec_i_ec;
 // CHECK: @s_ec_i_ec = addrspace(3) global %struct.EC_I_EC undef,
 __constant__ EC_I_EC c_ec_i_ec;
-// NVPTX: @c_ec_i_ec = addrspace(4) externally_initialized global %struct.EC_I_EC zeroinitializer,
-// AMDGCN: @c_ec_i_ec = addrspace(2) externally_initialized global %struct.EC_I_EC zeroinitializer,
+// CHECK: @c_ec_i_ec = addrspace(4) externally_initialized global %struct.EC_I_EC zeroinitializer,
 
 // We should not emit global initializers for device-side variables.
 // CHECK-NOT: @__cxx_global_var_init
@@ -129,53 +117,41 @@ __device__ void df() {
   T t;
   // CHECK-NOT: call
   EC ec;
-  // NVPTX:   call void @_ZN2ECC1Ev(%struct.EC* %ec)
-  // AMDGCN:   call void @_ZN2ECC1Ev(%struct.EC addrspace(4)* %ec)
+  // CHECK:   call void @_ZN2ECC1Ev(%struct.EC* %ec)
   ED ed;
   // CHECK-NOT: call
   ECD ecd;
-  // NVPTX:   call void @_ZN3ECDC1Ev(%struct.ECD* %ecd)
-  // AMDGCN:   call void @_ZN3ECDC1Ev(%struct.ECD addrspace(4)* %ecd)
+  // CHECK:   call void @_ZN3ECDC1Ev(%struct.ECD* %ecd)
   ETC etc;
-  // NVPTX:   call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC* %etc)
-  // AMDGCN:   call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC addrspace(4)* %etc)
+  // CHECK:   call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC* %etc)
   UC uc;
   // undefined constructor -- not allowed
-  // NVPTX:   call void @_ZN2UCC1Ev(%struct.UC* %uc)
-  // AMDGCN:   call void @_ZN2UCC1Ev(%struct.UC addrspace(4)* %uc)
+  // CHECK:   call void @_ZN2UCC1Ev(%struct.UC* %uc)
   UD ud;
   // undefined destructor -- not allowed
   // CHECK-NOT: call
   ECI eci;
   // empty constructor w/ initializer list -- not allowed
-  // NVPTX:   call void @_ZN3ECIC1Ev(%struct.ECI* %eci)
-  // AMDGCN:   call void @_ZN3ECIC1Ev(%struct.ECI addrspace(4)* %eci)
+  // CHECK:   call void @_ZN3ECIC1Ev(%struct.ECI* %eci)
   NEC nec;
   // non-empty constructor -- not allowed
-  // NVPTX:   call void @_ZN3NECC1Ev(%struct.NEC* %nec)
-  // AMDGCN:   call void @_ZN3NECC1Ev(%struct.NEC addrspace(4)* %nec)
+  // CHECK:   call void @_ZN3NECC1Ev(%struct.NEC* %nec)
   // non-empty destructor -- not allowed
   NED ned;
   // no-constructor,  virtual method -- not allowed
-  // NVPTX:   call void @_ZN3NCVC1Ev(%struct.NCV* %ncv)
-  // AMDGCN:   call void @_ZN3NCVC1Ev(%struct.NCV addrspace(4)* %ncv)
+  // CHECK:   call void @_ZN3NCVC1Ev(%struct.NCV* %ncv)
   NCV ncv;
   // CHECK-NOT: call
   VD vd;
-  // NVPTX:   call void @_ZN2VDC1Ev(%struct.VD* %vd)
-  // AMDGCN:   call void @_ZN2VDC1Ev(%struct.VD addrspace(4)* %vd)
+  // CHECK:   call void @_ZN2VDC1Ev(%struct.VD* %vd)
   NCF ncf;
-  // NVPTX:   call void @_ZN3NCFC1Ev(%struct.NCF* %ncf)
-  // AMDGCN:   call void @_ZN3NCFC1Ev(%struct.NCF addrspace(4)* %ncf)
+  // CHECK:   call void @_ZN3NCFC1Ev(%struct.NCF* %ncf)
   NCFS ncfs;
-  // NVPTX:   call void @_ZN4NCFSC1Ev(%struct.NCFS* %ncfs)
-  // AMDGCN:   call void @_ZN4NCFSC1Ev(%struct.NCFS addrspace(4)* %ncfs)
+  // CHECK:   call void @_ZN4NCFSC1Ev(%struct.NCFS* %ncfs)
   UTC utc;
-  // NVPTX:   call void @_ZN3UTCC1IJEEEDpT_(%struct.UTC* %utc)
-  // AMDGCN:   call void @_ZN3UTCC1IJEEEDpT_(%struct.UTC addrspace(4)* %utc)
+  // CHECK:   call void @_ZN3UTCC1IJEEEDpT_(%struct.UTC* %utc)
   NETC netc;
-  // NVPTX:   call void @_ZN4NETCC1IJEEEDpT_(%struct.NETC* %netc)
-  // AMDGCN:   call void @_ZN4NETCC1IJEEEDpT_(%struct.NETC addrspace(4)* %netc)
+  // CHECK:   call void @_ZN4NETCC1IJEEEDpT_(%struct.NETC* %netc)
   T_B_T t_b_t;
   // CHECK-NOT: call
   T_F_T t_f_t;
@@ -183,23 +159,17 @@ __device__ void df() {
   T_FA_T t_fa_t;
   // CHECK-NOT: call
   EC_I_EC ec_i_ec;
-  // NVPTX:   call void @_ZN7EC_I_ECC1Ev(%struct.EC_I_EC* %ec_i_ec)
-  // AMDGCN:   call void @_ZN7EC_I_ECC1Ev(%struct.EC_I_EC addrspace(4)* %ec_i_ec)
+  // CHECK:   call void @_ZN7EC_I_ECC1Ev(%struct.EC_I_EC* %ec_i_ec)
   EC_I_EC1 ec_i_ec1;
-  // NVPTX:   call void @_ZN8EC_I_EC1C1Ev(%struct.EC_I_EC1* %ec_i_ec1)
-  // AMDGCN:   call void @_ZN8EC_I_EC1C1Ev(%struct.EC_I_EC1 addrspace(4)* %ec_i_ec1)
+  // CHECK:   call void @_ZN8EC_I_EC1C1Ev(%struct.EC_I_EC1* %ec_i_ec1)
   T_V_T t_v_t;
-  // NVPTX:   call void @_ZN5T_V_TC1Ev(%struct.T_V_T* %t_v_t)
-  // AMDGCN:   call void @_ZN5T_V_TC1Ev(%struct.T_V_T addrspace(4)* %t_v_t)
+  // CHECK:   call void @_ZN5T_V_TC1Ev(%struct.T_V_T* %t_v_t)
   T_B_NEC t_b_nec;
-  // NVPTX:   call void @_ZN7T_B_NECC1Ev(%struct.T_B_NEC* %t_b_nec)
-  // AMDGCN:   call void @_ZN7T_B_NECC1Ev(%struct.T_B_NEC addrspace(4)* %t_b_nec)
+  // CHECK:   call void @_ZN7T_B_NECC1Ev(%struct.T_B_NEC* %t_b_nec)
   T_F_NEC t_f_nec;
-  // NVPTX:   call void @_ZN7T_F_NECC1Ev(%struct.T_F_NEC* %t_f_nec)
-  // AMDGCN:   call void @_ZN7T_F_NECC1Ev(%struct.T_F_NEC addrspace(4)* %t_f_nec)
+  // CHECK:   call void @_ZN7T_F_NECC1Ev(%struct.T_F_NEC* %t_f_nec)
   T_FA_NEC t_fa_nec;
-  // NVPTX:   call void @_ZN8T_FA_NECC1Ev(%struct.T_FA_NEC* %t_fa_nec)
-  // AMDGCN:   call void @_ZN8T_FA_NECC1Ev(%struct.T_FA_NEC addrspace(4)* %t_fa_nec)
+  // CHECK:   call void @_ZN8T_FA_NECC1Ev(%struct.T_FA_NEC* %t_fa_nec)
   T_B_NED t_b_ned;
   // CHECK-NOT: call
   T_F_NED t_f_ned;
@@ -207,33 +177,22 @@ __device__ void df() {
   T_FA_NED t_fa_ned;
   // CHECK-NOT: call
   static __shared__ EC s_ec;
-  // NVPTX-NOT: call void @_ZN2ECC1Ev(%struct.EC* addrspacecast (%struct.EC addrspace(3)* @_ZZ2dfvE4s_ec to %struct.EC*))
-  // AMDGCN-NOT: call void @_ZN2ECC1Ev(%struct.EC addrspace(4)* addrspacecast (%struct.EC addrspace(3)* @_ZZ2dfvE4s_ec to %struct.EC addrspace(4)*))
+  // CHECK-NOT: call void @_ZN2ECC1Ev(%struct.EC* addrspacecast (%struct.EC addrspace(3)* @_ZZ2dfvE4s_ec to %struct.EC*))
   static __shared__ ETC s_etc;
-  // NVPTX-NOT: call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC* addrspacecast (%struct.ETC addrspace(3)* @_ZZ2dfvE5s_etc to %struct.ETC*))
-  // AMDGCN-NOT: call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC addrspace(4)* addrspacecast (%struct.ETC addrspace(3)* @_ZZ2dfvE5s_etc to %struct.ETC addrspace(4)*))
+  // CHECK-NOT: call void @_ZN3ETCC1IJEEEDpT_(%struct.ETC* addrspacecast (%struct.ETC addrspace(3)* @_ZZ2dfvE5s_etc to %struct.ETC*))
 
   // anchor point separating constructors and destructors
   df(); // CHECK: call void @_Z2dfv()
 
   // Verify that we only call non-empty destructors
-  // NVPTX-NEXT: call void @_ZN8T_FA_NEDD1Ev(%struct.T_FA_NED* %t_fa_ned)
-  // NVPTX-NEXT: call void @_ZN7T_F_NEDD1Ev(%struct.T_F_NED* %t_f_ned)
-  // NVPTX-NEXT: call void @_ZN7T_B_NEDD1Ev(%struct.T_B_NED* %t_b_ned)
-  // NVPTX-NEXT: call void @_ZN2VDD1Ev(%struct.VD* %vd)
-  // NVPTX-NEXT: call void @_ZN3NEDD1Ev(%struct.NED* %ned)
-  // NVPTX-NEXT: call void @_ZN2UDD1Ev(%struct.UD* %ud)
-  // NVPTX-NEXT: call void @_ZN3ECDD1Ev(%struct.ECD* %ecd)
-  // NVPTX-NEXT: call void @_ZN2EDD1Ev(%struct.ED* %ed)
-
-  // AMDGCN-NEXT: call void @_ZN8T_FA_NEDD1Ev(%struct.T_FA_NED addrspace(4)* %t_fa_ned)
-  // AMDGCN-NEXT: call void @_ZN7T_F_NEDD1Ev(%struct.T_F_NED addrspace(4)* %t_f_ned)
-  // AMDGCN-NEXT: call void @_ZN7T_B_NEDD1Ev(%struct.T_B_NED addrspace(4)* %t_b_ned)
-  // AMDGCN-NEXT: call void @_ZN2VDD1Ev(%struct.VD addrspace(4)* %vd)
-  // AMDGCN-NEXT: call void @_ZN3NEDD1Ev(%struct.NED addrspace(4)* %ned)
-  // AMDGCN-NEXT: call void @_ZN2UDD1Ev(%struct.UD addrspace(4)* %ud)
-  // AMDGCN-NEXT: call void @_ZN3ECDD1Ev(%struct.ECD addrspace(4)* %ecd)
-  // AMDGCN-NEXT: call void @_ZN2EDD1Ev(%struct.ED addrspace(4)* %ed)
+  // CHECK-NEXT: call void @_ZN8T_FA_NEDD1Ev(%struct.T_FA_NED* %t_fa_ned)
+  // CHECK-NEXT: call void @_ZN7T_F_NEDD1Ev(%struct.T_F_NED* %t_f_ned)
+  // CHECK-NEXT: call void @_ZN7T_B_NEDD1Ev(%struct.T_B_NED* %t_b_ned)
+  // CHECK-NEXT: call void @_ZN2VDD1Ev(%struct.VD* %vd)
+  // CHECK-NEXT: call void @_ZN3NEDD1Ev(%struct.NED* %ned)
+  // CHECK-NEXT: call void @_ZN2UDD1Ev(%struct.UD* %ud)
+  // CHECK-NEXT: call void @_ZN3ECDD1Ev(%struct.ECD* %ecd)
+  // CHECK-NEXT: call void @_ZN2EDD1Ev(%struct.ED* %ed)
 
   // CHECK-NEXT: ret void
 }
