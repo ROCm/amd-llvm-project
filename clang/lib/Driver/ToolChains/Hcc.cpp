264ced6d9f1b2dc6ead397d8b3c8aad9dd247488
Sameer Sahasrabuddhe <sameer.sahasrabuddhe@amd.com>
2019-10-11 10:59:47 +0530

    update IncPath to reflect the change to llvm-project
    When moving to llvm-project, the Clang sources moved from hcc/clang to
    hcc/llvm-project/clang. This added an extra directory to the Clang
    Driver's "BinPath" property. The driver now needs to traverse an extra
    level upwards to locate the include directory.

--- clang/lib/Driver/ToolChains/Hcc.cpp
+++ clang/lib/Driver/ToolChains/Hcc.cpp
@@ -42,6 +42,7 @@ HCCInstallationDetector::HCCInstallationDetector(const Driver &D, const llvm::op
   HCCPathCandidates.push_back(InstallPath + "/..");
   HCCPathCandidates.push_back(BinPath + "/..");
   HCCPathCandidates.push_back(BinPath + "/../..");
+  HCCPathCandidates.push_back(BinPath + "/../../..");
 
   for (const auto &HCCPath: HCCPathCandidates) {
     if (HCCPath.empty() ||
