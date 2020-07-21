bbe1f57ae12b8bf3617637b851808c26cc166efd
Greg Rodgers <gregrodgers@users.noreply.github.com>
2020-07-21 13:19:20 -0500

    remove hostrpc fallback functions from libomptarget.  They are alreadh in libhostrpc.a.  Also, to find them we need to add -lhostrpc to host linker not only for device linking but final link (#102)

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -653,8 +653,7 @@ bool tools::addOpenMPRuntime(ArgStringList &CmdArgs, const ToolChain &TC,
       CmdArgs.push_back("-lrt");
 
   if (IsOffloadingHost) {
-    if (TC.getTriple().isAMDGCN())
-      CmdArgs.push_back("-lhostrpc");
+    CmdArgs.push_back("-lhostrpc");
     CmdArgs.push_back("-lomptarget");
   }
 
