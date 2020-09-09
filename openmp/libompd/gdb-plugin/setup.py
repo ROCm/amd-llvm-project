662987f947600b04d72b809461d7ad090bdd0b86
estewart08 <estewart08@gmail.com>
2020-09-09 13:56:22 -0500

    Fixes for Red Hat build. (#157)
    The --system flag is not applicable to non-debian operating systems (change to cmake).
    
    Red Hat 7.6 and lower do not provide a python36-devel epel repository.
    Therefore, a software collection needs installed at /opt/rh/rh-python36.
    The setup.py needs to pass this new python header location or Python.h will not be found.
    
    Co-authored-by: Ethan Stewart <ethan.stewart@amd.com>

--- openmp/libompd/gdb-plugin/setup.py
+++ openmp/libompd/gdb-plugin/setup.py
@@ -4,6 +4,7 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 omp_include_dir = os.environ.get('LIBOMP_INCLUDE_DIR', dir_path)
+python_include_dir = os.environ.get('PYTHON_HEADERS', dir_path)
 
 print("find_packages : ", find_packages())
 setup(
@@ -11,5 +12,5 @@ setup(
     version='1.0',
     py_modules=['loadompd'],
     packages=find_packages(),
-	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c'], include_dirs=[omp_include_dir])]
+	ext_modules=[Extension('ompd.ompdModule', [dir_path+'/ompdModule.c'], include_dirs=[omp_include_dir, python_include_dir])]
 )
