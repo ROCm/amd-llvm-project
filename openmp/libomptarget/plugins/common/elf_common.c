7bfd8512f1ccbd369e22ab52bc8dcbfaf76b7415
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-12-04 14:32:54 +0000

    Refactor elfutil

--- openmp/libomptarget/plugins/common/elf_common.c
+++ openmp/libomptarget/plugins/common/elf_common.c
@@ -13,9 +13,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if !(defined(_OMPTARGET_H_) && defined(DP))
-#error Include elf_common.c in the plugin source AFTER omptarget.h has been\
- included and macro DP(...) has been defined.
+#if !(defined(_OMPTARGET_DEBUG_H))
+#error Include elf_common.c in the plugin source AFTER Debug.h has\
+ been included.
 #endif
 
 #include <elf.h>
@@ -108,42 +108,4 @@ static inline int32_t elf_is_dynamic(__tgt_device_image *image) {
   elf_end(e);
   DP("ELF Type: %d\n", Type);
   return Type == ET_DYN;
-}
-
-static inline uint32_t elf_e_flags(__tgt_device_image *image) {
-
-  char *img_begin = (char *)image->ImageStart;
-  char *img_end = (char *)image->ImageEnd;
-  size_t img_size = img_end - img_begin;
-
-  // Obtain elf handler
-  Elf *e = elf_memory(img_begin, img_size);
-  if (!e) {
-    DP("Unable to get ELF handle: %s!\n", elf_errmsg(-1));
-    return 0;
-  }
-
-  Elf64_Ehdr *eh64 = elf64_getehdr(e);
-  Elf32_Ehdr *eh32 = elf32_getehdr(e);
-
-  if (!eh64 && !eh32) {
-    DP("Unable to get machine ID from ELF file!\n");
-    elf_end(e);
-    return 0;
-  }
-
-  uint32_t Flags;
-  if (eh64 && !eh32)
-    Flags = eh64->e_flags;
-  else if (eh32 && !eh64)
-    Flags = eh32->e_flags;
-  else {
-    DP("Ambiguous ELF header!\n");
-    elf_end(e);
-    return 0;
-  }
-
-  elf_end(e);
-  DP("ELF Flags: 0x%x\n", Flags);
-  return Flags;
 }
